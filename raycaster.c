a79ab55f32553a3946c20acb369c84f60862f99c
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-05 15:13:46 -0700

    Raycaster works now

--- raycaster.c
+++ raycaster.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <stdio.h>
 
 #define PI 3.1415926535
 #define P2 PI/2
 #define P3 3*PI/2
+#define DR 0.0174533 //one degree in radians
 
 float px, py, pdx, pdy, pa;
 
@@ -51,44 +53,57 @@ void drawMap2D() {
      }
 }
 
+float dist(float ax,float ay, float bx, float by, float ang){
+     return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)));
+}
+
 void drawRays2D() {
      int r, mx, my, mp, dof;
-     float rx, ry, ra, xo, yo;
-     ra = pa;
-     for (r = 0; r < 1;r++){
+     float rx, ry, ra, xo, yo, disT;
+     ra = pa-DR*30; if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;}
+     for (r = 0; r < 90;r++){
           dof = 0;
+          float disH = 1000000, hx = px, hy = py;
           float aTan = -1 / tan(ra);
           if(ra>PI){ry=(((int)py>>6)<<6)-0.0001; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan;}
           if(ra<PI){ry=(((int)py>>6)<<6)+64; rx=(py-ry)*aTan+px; yo=64; xo=-yo*aTan;}
           if(ra==0 || ra==PI){rx=px; ry=py; dof=8;}
           while(dof<8){
                mx = (int)(rx) >> 6; my = (int)(ry)>>6; mp=my*mapX+mx;
-               if(mp<mapX*mapY && map[mp]==1){dof=8;}
+               if(mp>0 && mp<mapX*mapY && map[mp]==1){hx=rx; hy=ry; disH=dist(px,py,hx,hy,ra); dof=8;}
                else{rx+=xo; ry+=yo;dof+=1;}
           }
-          glColor3f(0, 1, 0);
-          glLineWidth(10);
-          glBegin(GL_LINES);
-          glVertex2i(px, py);
-          glVertex2i(rx, ry);
-          glEnd();
 
           dof = 0;
+          float disV = 1000000, vx = px, vy = py;
           float nTan = -tan(ra);
           if(ra>P2 && ra<P3){rx=(((int)px>>6)<<6)-0.0001; ry=(px-rx)*nTan+py; xo=-64; yo=-xo*nTan;}
           if(ra<P2 || ra>P3){rx=(((int)px>>6)<<6)+64; ry=(px-rx)*nTan+py; xo=64; yo=-xo*nTan;}
           if(ra==0 || ra==PI){rx=px; ry=py; dof=8;}
           while(dof<8){
                mx = (int)(rx) >> 6; my = (int)(ry)>>6; mp=my*mapX+mx;
-               if(mp<mapX*mapY && map[mp]==1){dof=8;}
+               if(mp>0 && mp<mapX*mapY && map[mp]==1){vx=rx; vy=ry; disV=dist(px,py,vx,vy,ra); dof=8;}
                else{rx+=xo; ry+=yo;dof+=1;}
           }
+          if(disV<disH){rx=vx; ry=vy; disT=disV;}
+          if(disH<disV){rx=hx; ry=hy; disT=disH;}
           glColor3f(1, 0, 0);
           glLineWidth(3);
           glBegin(GL_LINES);
           glVertex2i(px, py);
           glVertex2i(rx, ry);
           glEnd();
+
+          int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
+          int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
+
+          float ca=pa-ra; if(ca<0) {ca+=2*PI;} if(ca>2*PI) {ca-=2*PI;} disT=disT*cos(ca);
+          float lineH = (mapS * screenHeight)/disT; if(lineH>screenHeight){lineH=screenHeight;}
+          float lineO=(screenHeight/4)-lineH/2;
+          glLineWidth(16);glBegin(GL_LINES);glVertex2i(r*8+530,lineO);glVertex2i(r*8+530,lineH+lineO);
+          glEnd();
+
+          ra += DR; if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;}
      }
 }
 
@@ -113,15 +128,16 @@ void init() {
      gluOrtho2D(0, 1024, 512, 0);
      px = 300;
      py = 300;
-     pa = 1;
      pdx = cos(pa) * 5;
      pdy = sin(pa) * 5;
 }
 
 int main(int argc, char* argv[]){
      glutInit(&argc, argv);
+     int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
+     int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
      glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-     glutInitWindowSize(1024, 512);
+     glutInitWindowSize(screenWidth, screenHeight);
      glutCreateWindow("KobiWare 2.5D Engine");
      init();
      glutDisplayFunc(display);
