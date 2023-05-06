f1ad11d3f3ab7f51e3f5cf418c75533b1044b0fd
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-05 23:06:44 -0700

    commenting it up somewhat

--- raycaster.c
+++ raycaster.c
@@ -8,33 +8,28 @@
 #define P3 3*PI/2
 #define DR 0.0174533 //one degree in radians
 
-float px, py, pdx, pdy, pa;
+float px, py, pdx, pdy, pa; //player xy, player delta xy, player angle
 
 void drawPlayer(){
      glColor3f(1, 1, 0);
      glPointSize(8);
      glBegin(GL_POINTS);
      glVertex2i(px, py);
-     glEnd();
+     glEnd(); //draw yellow dot representing player
 
      glLineWidth(3);
      glBegin(GL_LINES);
      glVertex2i(px, py);
-     glVertex2i(px + pdx * 5, py + pdy * 5);
+     glVertex2i(px + pdx * 5, py + pdy * 5); //draw thing that casts rays out
      glEnd();
 }
 
-typedef struct{
-     int w, a, d, s;
-} ButtonKeys; ButtonKeys Keys;
-
-int mapX = 8,
-    mapY = 8, mapS = 64;
+int mapX = 8, mapY = 8, mapS = 64;
 int map[]= {
      1,1,1,1,1,1,1,1,
      1,0,1,0,0,0,0,1,
      1,0,1,0,0,0,0,1,
-     1,0,1,0,0,0,1,1,
+     1,0,1,0,0,0,0,1,
      1,0,0,0,0,0,0,1,
      1,0,0,0,0,1,0,1,
      1,0,0,0,0,0,0,1,
@@ -45,34 +40,41 @@ void drawMap2D() {
      int x, y, xo, yo;
      for (y = 0; y < mapY;y++){
           for (x = 0; x < mapX; x++){
-               if(map[y*mapX+x]==1){glColor3f(1,1,1);} else {glColor3f(0,0,0);}
-               xo = x * mapS;
-               yo = y * mapS;
+               if(map[y*mapX+x]==1){glColor3f(1,1,1);} else {glColor3f(0,0,0);} //loops thjrough every coordinate and sets the 2d box according to if box is 1 or 0
+               xo = x * mapS; //x offset is equal to x multiplied by the full map size
+               yo = y * mapS; //y offset is equal to y multiplied by the full map size
                glBegin(GL_QUADS);
                glVertex2i(xo+1, yo+1);
                glVertex2i(xo+1, yo + mapS-1);
                glVertex2i(xo-1 + mapS, yo + mapS-1);
-               glVertex2i(xo-1 + mapS, yo+1);
+               glVertex2i(xo-1 + mapS, yo+1); //create grid like look to the 2d map
                glEnd();
           }
      }
 }
 
 float dist(float ax,float ay, float bx, float by, float ang){
-     return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)));
+     return (sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay))); //calculate the distance 
 }
 
 void drawRays2D() {
      int r, mx, my, mp, dof;
      float rx, ry, ra, xo, yo, disT;
-     ra = pa-DR*30; if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;}
-     for (r = 0; r < 90;r++){
-          dof = 0;
-          float disH = 1000000, hx = px, hy = py;
-          float aTan = -1 / tan(ra);
-          if(ra>PI){ry=(((int)py>>6)<<6)-0.0001; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan;}
-          if(ra<PI){ry=(((int)py>>6)<<6)+64; rx=(py-ry)*aTan+px; yo=64; xo=-yo*aTan;}
-          if(ra==0 || ra==PI){rx=px; ry=py; dof=8;}
+     ra = pa-DR*30; //convert ray angle to player angle minus 30 degrees in radians
+     if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;} //if radians is less than zero, add 2 pi, if greater subtract 2 pi
+     for (r = 0; r < 90;r++){ //run through for each ray
+          dof = 0; //depth of field is equal to 0 at beginning
+          float disH = 1000000, hx = px, hy = py; //horizontal x and horizontal y are equal to current player x and y
+          float aTan = -1 / tan(ra); //angle tangent is equal to -1 / tan(ra)
+          if(ra>PI){ry=(((int)py>>6)<<6)-0.0001; //if the ray angle is greater than pi, ray y is equal to ((player y / 64)*64)-0.0001
+          rx=(py-ry)*aTan+px;  //ray x is equal to (player y - ray y) multiplied by aTan and then adding player x
+          yo=-64; //y offset is equal to -64
+          xo=-yo*aTan;} //xo offset is equal to negative y offset multiplied by aTan
+          if(ra<PI){ry=(((int)py>>6)<<6)+64;  //if ray angle is less than pi, ray y is equal to py / 64 * 64 + 64
+          rx=(py-ry)*aTan+px;  //ray x is equal to player y - ray y multiplied by aTan plus player x
+          yo=64; //y offset is equal to 64
+          xo=-yo*aTan;} //x offset is equal to negative y offset multiplied by aTan
+          if(ra==0 || ra==PI){rx=px; ry=py; dof=8;} //if the ray angle is 0 or PI, ray x is equal to player x and ry is equal to player 1 and dof is equal to 8
           while(dof<8){
                mx = (int)(rx) >> 6; my = (int)(ry)>>6; mp=my*mapX+mx;
                if(mp>0 && mp<mapX*mapY && map[mp]==1){hx=rx; hy=ry; disH=dist(px,py,hx,hy,ra); dof=8;}
@@ -90,13 +92,14 @@ void drawRays2D() {
                if(mp>0 && mp<mapX*mapY && map[mp]==1){vx=rx; vy=ry; disV=dist(px,py,vx,vy,ra); dof=8;}
                else{rx+=xo; ry+=yo;dof+=1;}
           }
-          if(disV<disH){rx=vx; ry=vy; disT=disV; glColor3f(0.9,0.9,0);}
-          if(disH<disV){rx=hx; ry=hy; disT=disH; glColor3f(0.7,0.7,0);}
-          //glLineWidth(3);
-          //glBegin(GL_LINES);
-          //glVertex2i(px, py);
-          //glVertex2i(rx, ry);
-          //glEnd();
+          if(disV<disH){rx=vx; ry=vy; disT=disV;}
+          if(disH<disV){rx=hx; ry=hy; disT=disH;}
+          glColor3f(1, 0, 0);
+          glLineWidth(3);
+          glBegin(GL_LINES);
+          glVertex2i(px, py);
+          glVertex2i(rx, ry);
+          glEnd();
 
           int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
           int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
@@ -104,68 +107,36 @@ void drawRays2D() {
           float ca=pa-ra; if(ca<0) {ca+=2*PI;} if(ca>2*PI) {ca-=2*PI;} disT=disT*cos(ca);
           float lineH = (mapS * screenHeight)/disT; if(lineH>screenHeight){lineH=screenHeight;}
           float lineO=(screenHeight/4)-lineH/2;
-          glLineWidth(32);glBegin(GL_LINES);glVertex2i(r*16,lineO);glVertex2i(r*16,lineH+lineO);
+          glLineWidth(16);glBegin(GL_LINES);glVertex2i(r*8+530,lineO);glVertex2i(r*8+530,lineH+lineO);
           glEnd();
 
           ra += DR; if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;}
      }
 }
 
-void display(unsigned char key, int x, int y) {
-     if(Keys.a==1) {pa-=0.1; if(pa<=0){pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
-     if(Keys.d==1) {pa+=0.1; if(pa>2*PI){pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
-
-     int xo = 0; if(pdx<0){xo-=20;}else{xo=20;}
-     int yo = 0; if(pdy<0){yo-=20;}else{yo=20;}
-     int ipx = px / 64.0, ipx_add_xo = (px + xo) / 64.0, ipx_sub_xo = (px - xo) / 64.0;
-     int ipy = py / 64.0, ipy_add_yo = (py + yo) / 64.0, ipy_sub_yo = (py - yo) / 64.0;
-
-     if(Keys.w==1) {
-          if(map[ipy*mapX + ipx_add_xo]==0){px+=pdx; py+=pdy;}
-          if(map[ipy_add_yo*mapX + ipx]==0){py+=pdy; py+=pdy;}
-     }
-     if(Keys.s==1) {
-          if(map[ipy*mapX + ipx_sub_xo]==0){px-=pdx; py-=pdy;}
-          if(map[ipy_sub_yo*mapX + ipx]==0){py-=pdy; py-=pdy;}
-     }
-     glutPostRedisplay();
-
+void display() {
      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-     //drawMap2D();
-     //drawPlayer();
+     drawMap2D();
+     drawPlayer();
      drawRays2D();
      glutSwapBuffers();
 }
 
+void buttons(unsigned char key, int x, int y){
+     if(key=='a') {pa-=0.1; if(pa<=0){pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+     if(key=='d') {pa+=0.1; if(pa>2*PI){pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+     if(key=='w') {px+=pdx; py+=pdy;}
+     if(key=='s') {px-=pdx; py-=pdy;}
+     glutPostRedisplay();
+}
+
 void init() {
      glClearColor(0.3, 0.3, 0.3, 0);
-     gluOrtho2D(0, 1024, 510, 0);
+     gluOrtho2D(0, 1024, 512, 0);
      px = 300;
      py = 300;
      pdx = cos(pa) * 5;
      pdy = sin(pa) * 5;
-}
-
-void ButtonDown(unsigned char key, int x, int y){
-     if(key=='a'){Keys.a=1;}
-     if(key=='d'){Keys.d=1;}
-     if(key=='w'){Keys.w=1;}
-     if(key=='s'){Keys.s=1;}
-     if(key==27){glutDestroyWindow(1);}
-     glutPostRedisplay();
-}
-
-void ButtonUp(unsigned char key, int x, int y) {
-     if(key=='a'){Keys.a=0;}
-     if(key=='d'){Keys.d=0;}
-     if(key=='w'){Keys.w=0;}
-     if(key=='s'){Keys.s=0;}
-     if(key==27){glutDestroyWindow(1);}
-     glutPostRedisplay();
-}
-
-void resize(int w, int h){
-     return 0;
 }
 
 int main(int argc, char* argv[]){
@@ -173,13 +144,10 @@ int main(int argc, char* argv[]){
      int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
      int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
      glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+     glutInitWindowSize(screenWidth, screenHeight);
      glutCreateWindow("KobiWare 2.5D Engine");
-     glutFullScreen();
-     glViewport(0, 0, 512, 360);
      init();
      glutDisplayFunc(display);
-     glutReshapeFunc(resize);
-     glutKeyboardFunc(ButtonDown);
-     glutKeyboardUpFunc(ButtonUp);
+     glutKeyboardFunc(buttons);
      glutMainLoop();
 }
