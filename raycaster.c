f249957803a971881350ebc1c7db667df2a6484e
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-05-05 20:26:37 -0700

    collision

--- raycaster.c
+++ raycaster.c
@@ -24,12 +24,17 @@ void drawPlayer(){
      glEnd();
 }
 
-int mapX = 8, mapY = 8, mapS = 64;
+typedef struct{
+     int w, a, d, s;
+} ButtonKeys; ButtonKeys Keys;
+
+int mapX = 8,
+    mapY = 8, mapS = 64;
 int map[]= {
      1,1,1,1,1,1,1,1,
      1,0,1,0,0,0,0,1,
      1,0,1,0,0,0,0,1,
-     1,0,1,0,0,0,0,1,
+     1,0,1,0,0,0,1,1,
      1,0,0,0,0,0,0,1,
      1,0,0,0,0,1,0,1,
      1,0,0,0,0,0,0,1,
@@ -85,14 +90,13 @@ void drawRays2D() {
                if(mp>0 && mp<mapX*mapY && map[mp]==1){vx=rx; vy=ry; disV=dist(px,py,vx,vy,ra); dof=8;}
                else{rx+=xo; ry+=yo;dof+=1;}
           }
-          if(disV<disH){rx=vx; ry=vy; disT=disV;}
-          if(disH<disV){rx=hx; ry=hy; disT=disH;}
-          glColor3f(1, 0, 0);
-          glLineWidth(3);
-          glBegin(GL_LINES);
-          glVertex2i(px, py);
-          glVertex2i(rx, ry);
-          glEnd();
+          if(disV<disH){rx=vx; ry=vy; disT=disV; glColor3f(0.9,0.9,0);}
+          if(disH<disV){rx=hx; ry=hy; disT=disH; glColor3f(0.7,0.7,0);}
+          //glLineWidth(3);
+          //glBegin(GL_LINES);
+          //glVertex2i(px, py);
+          //glVertex2i(rx, ry);
+          //glEnd();
 
           int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
           int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
@@ -100,36 +104,68 @@ void drawRays2D() {
           float ca=pa-ra; if(ca<0) {ca+=2*PI;} if(ca>2*PI) {ca-=2*PI;} disT=disT*cos(ca);
           float lineH = (mapS * screenHeight)/disT; if(lineH>screenHeight){lineH=screenHeight;}
           float lineO=(screenHeight/4)-lineH/2;
-          glLineWidth(16);glBegin(GL_LINES);glVertex2i(r*8+530,lineO);glVertex2i(r*8+530,lineH+lineO);
+          glLineWidth(32);glBegin(GL_LINES);glVertex2i(r*16,lineO);glVertex2i(r*16,lineH+lineO);
           glEnd();
 
           ra += DR; if(ra<0) {ra+=2*PI;} if(ra>2*PI) {ra-=2*PI;}
      }
 }
 
-void display() {
+void display(unsigned char key, int x, int y) {
+     if(Keys.a==1) {pa-=0.1; if(pa<=0){pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+     if(Keys.d==1) {pa+=0.1; if(pa>2*PI){pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+
+     int xo = 0; if(pdx<0){xo-=20;}else{xo=20;}
+     int yo = 0; if(pdy<0){yo-=20;}else{yo=20;}
+     int ipx = px / 64.0, ipx_add_xo = (px + xo) / 64.0, ipx_sub_xo = (px - xo) / 64.0;
+     int ipy = py / 64.0, ipy_add_yo = (py + yo) / 64.0, ipy_sub_yo = (py - yo) / 64.0;
+
+     if(Keys.w==1) {
+          if(map[ipy*mapX + ipx_add_xo]==0){px+=pdx; py+=pdy;}
+          if(map[ipy_add_yo*mapX + ipx]==0){py+=pdy; py+=pdy;}
+     }
+     if(Keys.s==1) {
+          if(map[ipy*mapX + ipx_sub_xo]==0){px-=pdx; py-=pdy;}
+          if(map[ipy_sub_yo*mapX + ipx]==0){py-=pdy; py-=pdy;}
+     }
+     glutPostRedisplay();
+
      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-     drawMap2D();
-     drawPlayer();
+     //drawMap2D();
+     //drawPlayer();
      drawRays2D();
      glutSwapBuffers();
 }
 
-void buttons(unsigned char key, int x, int y){
-     if(key=='a') {pa-=0.1; if(pa<=0){pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
-     if(key=='d') {pa+=0.1; if(pa>2*PI){pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
-     if(key=='w') {px+=pdx; py+=pdy;}
-     if(key=='s') {px-=pdx; py-=pdy;}
-     glutPostRedisplay();
-}
-
 void init() {
      glClearColor(0.3, 0.3, 0.3, 0);
-     gluOrtho2D(0, 1024, 512, 0);
+     gluOrtho2D(0, 1024, 510, 0);
      px = 300;
      py = 300;
      pdx = cos(pa) * 5;
      pdy = sin(pa) * 5;
+}
+
+void ButtonDown(unsigned char key, int x, int y){
+     if(key=='a'){Keys.a=1;}
+     if(key=='d'){Keys.d=1;}
+     if(key=='w'){Keys.w=1;}
+     if(key=='s'){Keys.s=1;}
+     if(key==27){glutDestroyWindow(1);}
+     glutPostRedisplay();
+}
+
+void ButtonUp(unsigned char key, int x, int y) {
+     if(key=='a'){Keys.a=0;}
+     if(key=='d'){Keys.d=0;}
+     if(key=='w'){Keys.w=0;}
+     if(key=='s'){Keys.s=0;}
+     if(key==27){glutDestroyWindow(1);}
+     glutPostRedisplay();
+}
+
+void resize(int w, int h){
+     return 0;
 }
 
 int main(int argc, char* argv[]){
@@ -137,10 +173,13 @@ int main(int argc, char* argv[]){
      int screenWidth = glutGet(GLUT_SCREEN_WIDTH);
      int screenHeight = glutGet(GLUT_SCREEN_HEIGHT);
      glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-     glutInitWindowSize(screenWidth, screenHeight);
      glutCreateWindow("KobiWare 2.5D Engine");
+     glutFullScreen();
+     glViewport(0, 0, 512, 360);
      init();
      glutDisplayFunc(display);
-     glutKeyboardFunc(buttons);
+     glutReshapeFunc(resize);
+     glutKeyboardFunc(ButtonDown);
+     glutKeyboardUpFunc(ButtonUp);
      glutMainLoop();
 }
